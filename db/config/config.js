979a6d72eee8be1b0f1e10aa43c3c9e020a55554
Varun Kumar <varu1311@gmail.com>
2023-03-07 16:25:48 +0530

    feat: add migration docker container

--- db/config/config.js
+++ db/config/config.js
@@ -1,4 +1,20 @@
 module.exports = {
+  'docker': {
+    'username': process.env.DB_USER,
+    'password': process.env.DB_PASSWORD,
+    'database': process.env.DB_NAME,
+    'host': process.env.DB_HOST,
+    'dialect': 'postgres',
+    'port': process.env.DB_PORT,
+  },
+  'migration': {
+    'username': process.env.DB_USER,
+    'password': process.env.DB_PASSWORD,
+    'database': process.env.DB_NAME,
+    'host': process.env.DB_HOST,
+    'dialect': 'postgres',
+    'port': process.env.DB_PORT,
+  },
   'development': {
     'username': process.env.DB_USER,
     'password': process.env.DB_PASSWORD,
